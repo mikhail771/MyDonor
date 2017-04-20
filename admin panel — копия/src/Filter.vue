33c15d9fdfa380ca43e29a12b779aaa7e208e8cb
Roman <vizvoxer@gmail.com>
2017-04-20 10:58:20 +0300

    Add files via upload
    Как работать с админкой:
    1)Чтобы подтянуть зависимости в папке проекта - npm install.
    2)Запуск с Hot reload на  localhost:8080 - npm run dev

---
+++ admin panel — копия/src/Filter.vue
@@ -0,0 +1,32 @@
+<template>
+<ul>
+	<li class="head">Банк:</span><span> Адрес:</span></li>
+	<i-tems> </i-tems>
+</ul>
+</template>
+
+<script>
+import Items from './Item.vue'
+	export default{
+		 data: function(){
+    return{
+     keyword: ''
+    }
+  },
+  props: ['store', 'keyword'],
+		components:{
+			'i-tems': Items
+		},
+		  computed:{
+     filteredList(){
+        return this.store.filter((item) =>{
+          return item.bank.toLowerCase().includes(this.keyword.toLowerCase());
+        });
+      }
+  }
+	}
+</script>
+
+<style>
+	
+</style>
---
+++ admin panel — копия/src/List.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<i-tem v-for="item in 6"> </i-tem>
+	</div>
+</template>
+
+<script>
+	import Item from './Item.vue';
+export default {
+ components:{
+  'i-tem': Item
+ }
+ 
+}
+</script>
+
+<style>
+	
+</style>
