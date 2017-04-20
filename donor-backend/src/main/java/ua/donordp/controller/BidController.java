0f95ea81f4ee8375f4d67c3213de0de2d6fd34b4
Alexander Kotsar <kotsandroid@gmail.com>
2017-04-20 18:09:29 +0300

    Added method of adding a bid

--- donor-backend/src/main/java/ua/donordp/controller/BidController.java
+++ donor-backend/src/main/java/ua/donordp/controller/BidController.java
@@ -1,13 +1,16 @@
 package ua.donordp.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ua.donordp.model.Bid;
 import ua.donordp.service.BidService;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -27,4 +30,19 @@ public class BidController {
         return bidList;
     }
 
+    @RequestMapping(value = "/bids/add", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String addBid(@RequestBody String json) {
+
+        Bid bid = null;
+        try {
+            bid = new ObjectMapper().readValue(json, Bid.class);
+            this.bidService.addBid(bid);
+
+            return "Data successfully added!";
+
+        } catch (IOException e) {
+            return "ERROR JSON!";
+        }
+    }
 }
