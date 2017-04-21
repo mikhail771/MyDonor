ff68d741cb149554452d89c49217560d014334a4
Alexander Kotsar <kotsandroid@gmail.com>
2017-04-21 23:39:29 +0300

    Refactoring BidController

--- donor-backend/src/main/java/ua/donordp/controller/BidController.java
+++ donor-backend/src/main/java/ua/donordp/controller/BidController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ua.donordp.model.Bid;
 import ua.donordp.service.BidService;
@@ -13,7 +12,7 @@ import ua.donordp.service.BidService;
 import java.io.IOException;
 import java.util.List;
 
-@Controller
+@Controller(value = "/bids")
 public class BidController {
     private BidService bidService;
 
@@ -24,19 +23,17 @@ public class BidController {
     }
 
     @CrossOrigin
-    @RequestMapping(value = "/bids", method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     public @ResponseBody List<Bid> listBids(){
         List<Bid> bidList = this.bidService.listBids();
         return bidList;
     }
 
-    @RequestMapping(value = "/bids/add", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public String addBid(@RequestBody String json) {
-
-        Bid bid = null;
         try {
-            bid = new ObjectMapper().readValue(json, Bid.class);
+            Bid bid = new ObjectMapper().readValue(json, Bid.class);
             this.bidService.addBid(bid);
 
             return "Data successfully added!";
