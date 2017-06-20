5cc6918863387a5d162612b0dcfdf16e7f15f2e2
svolkovsky <svolkovsky.hillel@gmail.com>
2017-06-20 18:23:53 +0300

    secury fixes, incomplete

---
+++ donor-backend/src/main/java/ua/donordp/security/AuthenticationEntryPoint.java
@@ -0,0 +1,19 @@
+package ua.donordp.security;
+
+import org.springframework.security.core.AuthenticationException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by svolkovskyi on 20.06.17.
+ */
+
+public class AuthenticationEntryPoint implements org.springframework.security.web.AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Fuck you");
+    }
+}
---
+++ donor-backend/src/main/java/ua/donordp/security/AuthenticationSuccessHandler.java
@@ -0,0 +1,19 @@
+package ua.donordp.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by svolkovskyi on 20.06.17.
+ */
+public class AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        super.onAuthenticationSuccess(request, response, authentication);
+    }
+}
