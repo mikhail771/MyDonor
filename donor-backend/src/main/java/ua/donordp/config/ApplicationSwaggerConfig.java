32ee59bd27e6b9e7ff66bb91f25deb1ff717f808
hillel-java <svolkovsky.hillel@gmail.com>
2017-07-04 18:04:30 +0300

    organized spring config, added swagger

---
+++ donor-backend/src/main/java/ua/donordp/config/ApplicationSwaggerConfig.java
@@ -0,0 +1,26 @@
+package ua.donordp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by stephenvolf on 04/07/17.
+ */
+@Configuration
+@EnableSwagger2
+@ComponentScan("ua.donordp.controller")
+public class ApplicationSwaggerConfig {
+
+    @Bean
+    public Docket generalApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select().paths(PathSelectors.regex(".*"))
+                .build();
+    }
+
+}
